4e4256a56261a1c1dd28b9aa1d92226f5e4f8b7b
sabindice <sabin.bunea@gmail.com>
2021-11-17 00:10:11 +0200

    Fix all bugs

--- database/migrations/task1/2021_11_08_091231_create_tasks_table.php
+++ database/migrations/task1/2021_11_08_091231_create_tasks_table.php
@@ -15,6 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            //test
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
